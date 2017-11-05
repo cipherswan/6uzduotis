c51df465c9852e029ebd38afbc6361ecf8af155f
BuildTools <leskauskas25@gmail.com>
2017-11-05 13:58:35 +0200

    - update

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -24,6 +24,14 @@ public class Controller {
     public Label codeLabel;
     public TextField numberField;
     public TextArea textAreaNumbers;
+    public Label numberLabel;
+    public TextArea areaCurrency;
+    public TextField from;
+    public TextField amount;
+    public TextField to;
+    public TextArea quoteArea;
+    public Label labelAuthor;
+    public Label labelCategory;
 
     public void getCountry(ActionEvent actionEvent) throws UnirestException {
         String code = codeField.getText();
@@ -49,7 +57,39 @@ public class Controller {
     }
 
     public void getNumber(ActionEvent actionEvent) throws UnirestException {
+        String gNumber = numberField.getText();
+
+        HttpResponse<JsonNode> responseNumber = Unirest.get("https://numbersapi.p.mashape.com/"+gNumber+"/math?fragment=true&json=true")
+                .header("X-Mashape-Key", "F0NC8jv2KumshbvwF4mmMKEvBvBup1UyBMWjsnkrbTeV7elenc")
+                .header("Accept", "text/plain")
+                .asJson();
+
+        JSONObject myObj = responseNumber.getBody().getObject();
+        Integer number = myObj.getInt("number");
+        String text = myObj.getString("text");
+
+        numberLabel.setText(number.toString());
+        textAreaNumbers.setText(text);
 
 
+
+
+    }
+
+    public void getQuote(ActionEvent actionEvent) throws UnirestException {
+
+        HttpResponse<JsonNode> responseQuote = Unirest.get("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1")
+                .header("X-Mashape-Key", "F0NC8jv2KumshbvwF4mmMKEvBvBup1UyBMWjsnkrbTeV7elenc")
+                .header("Accept", "application/json")
+                .asJson();
+
+        JSONObject myObj = responseQuote.getBody().getObject();
+        String author = myObj.getString("author");
+        String category = myObj.getString("category");
+        String quote = myObj.getString("quote");
+
+        labelAuthor.setText(author);
+        labelCategory.setText(category);
+        quoteArea.setText(quote);
     }
 }
